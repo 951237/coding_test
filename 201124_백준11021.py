dbe021c439f0cfdd7825b4e285ddd8dba817dec4
951237 <44091528+951237@users.noreply.github.com>
2020-11-25 01:15:26 +0900

    Create 201124_백준11021.py
    - 빠른 합 / 결과를 예쁘게 출력하기

---
+++ 201124_백준11021.py
@@ -0,0 +1,14 @@
+# 빠른 합 / 결과를 예쁘게 출력
+import sys
+
+result = []
+a = int(input()) # 입력횟수 입력받기
+for i in range(a):
+  k = list(map(int, sys.stdin.readline().split())) # 여러줄을 입력받아 숫자로 바꿔서 리스트로 만들기
+  result.append(k)
+
+# print(result)
+
+for i in range(a):
+  sum = result[i][0] + result[i][1] # 입력받은 리스트 내의 리스트의 숫자의 합
+  print(f'Case #{i+1} : {sum}')
